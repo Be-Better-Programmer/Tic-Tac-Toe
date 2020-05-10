e1def345aa0146eb66907ed735f691569a8d16f0
hap@28 <harshitparikh2000@gmail.com>
2020-05-10 19:27:33 +0530

    HAP

--- app/src/main/java/com/bebetterprogrammer/tictactoe/MainActivity.kt
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.bebetterprogrammer.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playNow.setOnClickListener {
+            val intent = Intent(this, gamePage::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/bebetterprogrammer/tictactoe/gamePage.kt
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/gamePage.kt
@@ -1,12 +1,40 @@
 package com.bebetterprogrammer.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_game_page.*
 
 class gamePage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page)
+        var choose = 0
+        Obtn.setOnClickListener{
+            //O clicked
+             choose = 1
+            //startActivity(intent)
+
+        }
+        Xbtn.setOnClickListener{
+            //Xclicked
+             choose = 2
+            //startActivity(intent)
+
+        }
+
+
+        play.setOnClickListener {
+            val p1 = player1.text.toString()
+            val p2 = player2.text.toString()
+            val intent = Intent(this, Gameplay::class.java)
+            intent.putExtra("Player",choose.toString())
+            intent.putExtra("Player1",p1)
+            intent.putExtra("Player2",p2)
+
+            startActivity(intent)
+        }
+
     }
 }
