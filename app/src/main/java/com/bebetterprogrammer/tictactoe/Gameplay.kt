4eaeab8e494b45319f3e20d11b6392e0d941e21e
hap@28 <harshitparikh2000@gmail.com>
2020-05-10 19:19:59 +0530

    hap

---
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/Gameplay.kt
@@ -0,0 +1,12 @@
+package com.bebetterprogrammer.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Gameplay : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameplay)
+    }
+}
---
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/gamePage.kt
@@ -0,0 +1,12 @@
+package com.bebetterprogrammer.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gamePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_page)
+    }
+}
