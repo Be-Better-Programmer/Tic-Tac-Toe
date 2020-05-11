53ad96bf89faecc22cb771d92f581de22014f1a5
hap@28 <harshitparikh2000@gmail.com>
2020-05-11 22:06:24 +0530

    solve conflict 6

--- app/src/main/java/com/bebetterprogrammer/tictactoe/Gameplay.kt
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/Gameplay.kt
@@ -1,10 +1,10 @@
 package com.bebetterprogrammer.tictactoe
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.bebetterprogrammer.tictactoe.activities.HomePageActivity
 import kotlinx.android.synthetic.main.activity_gameplay.*
 
