4237ef1853d93bfa1455d26fcc152249c60132ae
hap@28 <harshitparikh2000@gmail.com>
2020-05-11 21:52:36 +0530

    solve conflict 5

--- app/src/main/java/com/bebetterprogrammer/tictactoe/Gameplay.kt
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/Gameplay.kt
@@ -1,4 +1,5 @@
 package com.bebetterprogrammer.tictactoe
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -54,6 +55,7 @@ class Gameplay : AppCompatActivity() {
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gameplay)
