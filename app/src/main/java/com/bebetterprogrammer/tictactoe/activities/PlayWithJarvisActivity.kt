4626c4583aa1324180eacd485a14500d4658a426
Umang Goti <umanggoti1@gmail.com>
2020-05-14 17:54:45 +0530

    Add play with jarvis UI and code implementation

--- app/src/main/java/com/bebetterprogrammer/tictactoe/activities/PlayWithJarvisActivity.kt
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/activities/PlayWithJarvisActivity.kt
@@ -1,8 +1,10 @@
 package com.bebetterprogrammer.tictactoe.activities
 
+import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bebetterprogrammer.tictactoe.BuildConfig
 import com.bebetterprogrammer.tictactoe.R
