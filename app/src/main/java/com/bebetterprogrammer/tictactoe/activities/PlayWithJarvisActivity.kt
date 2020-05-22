88185de41ede533e6aaaccfdde2b331a2281e0b3
Umang Goti <umanggoti1@gmail.com>
2020-05-22 12:21:48 +0530

    Added constraint layout for playwithjarvisactivity, playwithfriendactivity, and homepageactivity

--- app/src/main/java/com/bebetterprogrammer/tictactoe/activities/PlayWithJarvisActivity.kt
+++ app/src/main/java/com/bebetterprogrammer/tictactoe/activities/PlayWithJarvisActivity.kt
@@ -1,6 +1,7 @@
 package com.bebetterprogrammer.tictactoe.activities
 
 import android.content.Intent
+import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
